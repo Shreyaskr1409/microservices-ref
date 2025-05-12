0d27171897df24068fa49a9ce2e2812803fd44d3
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2025-05-12 23:15:39 +0530

    Leftovers from last commit

--- main.go
+++ main.go
@@ -1,43 +1,81 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Shreyaskr/microservices-ref/handlers"
 )
 
 func main() {
-	// there is a greedy-matching while matching the routes
-	// this means any route which is not mentioned in any of
-	// the HandleFunc will get routed to the function mentioned below
-	// http.ResponseWriter implements io.Writer
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// r.Body implements io.ReadCloser
-		d, err := io.ReadAll(r.Body)
-		if err != nil {
-			// w.WriteHeader(http.StatusBadRequest)
-			// w.Write([]byte("Error in reading the body"))
-			http.Error(w, "Error in reading the body", http.StatusBadRequest)
-			return
-		}
-		// %s will turn bytes into string
-		log.Printf("Hello %s\n", d)
-		// Fprintf formats data into a io.Writer
-		fmt.Fprintf(w, "Hello %s\n", d)
-	})
-	// the routes /bye/bye, /bye/world, etc. will not run the
-	// function below but will run if we change the string addr from
-	// "/bye" to "/bye/".
-	// this is usually how greedy matching in ServeMux runs
-	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
-	})
+	// Logger takes in io.Writer, prefix string and log flag
+	l := log.New(os.Stdout, "product-api: ", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewBye(l)
+
+	// Given below is the way Handler is implemented
+	//
+	// type Handler interface {
+	//     ServeHTTP(ResponseWriter, *Request)
+	// }
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/bye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
 	// ListenAndServe takes in a TCP network address and then calls
 	// Serve with handler to handle requests on incoming connections
 	// Unless someone specifier a new ServeMux, DefaultServeMux is
 	// used to Serve (Serve here is a function in http).
-	http.ListenAndServe(":9090", nil)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
 	// also remember that DefaultServeMux implements Handler interface
+
+	// -------------- BUFFERED vs/ UNBUFFERED CHANNELS ---------------
+	// Here we will get a warning for using an unbuffered channel.
+	// The reason for this is that goroutines take a bit
+	// of delay to check for channels. If the the goroutine is not
+	// immediately ready to handle those signals, the signal will get
+	// lost when we encounter another signal.
+	// while this is important for other implementations for the chan,
+	// we can move on with this since we are constantly blocking at few
+	// lines below.
+	// If we were using this for a 'for' loop, there might be sometimes
+	// when some function is happening and some time when the channel
+	// is checked
+	// if we had to use buffered channel (which we stil can), we would
+	// use:
+	// sigChan := make(chan os.Signal, 1)
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	signal.Notify(sigChan, os.Interrupt)
+
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shutdown due to signal: ", sig)
+
+	// timeout-context
+	// allows 30s to close all handlers
+	// tc is like a counter
+	// cancel is used to release resources
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	// shutdown everything forcefully if it is still working
+	l.Println("Shutdown started")
+	s.Shutdown(tc)
 }
